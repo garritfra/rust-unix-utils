873314063b3be8b4622d54547679dedb60f9de98
frankeg <garrit.franke@ckc.de>
2020-06-30 21:29:24 +0200

    Add simple cat implementation

---
+++ src/cat.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() >= 1 {
+        return concat_files(args);
+    }
+
+    Ok(())
+}
+
+fn concat_files(paths: Vec<String>) -> io::Result<()> {
+    for path in paths {
+        let mut file = match File::open(&path) {
+            Err(why) => panic!("couldn't open {}: {}", path, why),
+            Ok(file) => file,
+        };
+
+        let mut s = String::new();
+        match file.read_to_string(&mut s) {
+            Err(_) => {
+                continue;
+            }
+            Ok(_) => {
+                print!("{}", s);
+            }
+        }
+    }
+
+    Ok(())
+}
